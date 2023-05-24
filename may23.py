46461846f4f706bcaf1de44fc6bdd14d225acf21
Jackson Fellows <jtfellows@icloud.com>
2023-05-24 16:27:15 -0600

    Use station and shot data to calculate correct offsets.
    I used the Excel file of shot info that Lindsay sent me and a csv of
    station info that I dumped from Joshua's code. This allowed me to get
    the lat,lon for each shot and station and calculate correct
    source-receiver offsets. I plotted the shots I downloaded earlier and
    everything looked right to me.

--- may23.py
+++ may23.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import obspy
 import os
 import glob
+import pandas as pd
 
 
 def load_sample_stream():
@@ -68,31 +69,78 @@ def process_stream_2(ref_st):
         st[i].data /= np.max(np.abs(st[i].data))
     return st
 
-def plot_stream(st):
-    plt.gca().invert_xaxis()
+def plot_stream(st, show=True):
     red_vel = 6.0
     yy = np.arange(len(st[0])) / st[0].stats.sampling_rate
+    min_offset = float('inf')
+    max_offset = float('-inf')
     for i in range(len(st)):
         offset = st[i].stats.segy.trace_header.distance_from_center_of_the_source_point_to_the_center_of_the_receiver_group / 1000
+        min_offset = min(offset, min_offset)
+        max_offset = max(offset, max_offset)
         red_vel_shift = np.abs(offset) / red_vel
         x = st[i].data + offset
         y = yy - red_vel_shift
         z = np.zeros(len(x)) + offset
         plt.fill_betweenx(y, x, z, where=x > z, facecolor='k', zorder=1)
         plt.plot(x, y, 'k', linewidth=0.1)
+    plt.xlim(max_offset + 2, min_offset - 2)
     plt.ylim(0, 8)
-    plt.show()
+    if show:
+        plt.show()
 
-def load_shot(shotid):
-    shot_dir = os.path.join('shots', shotid)
+def load_shot(shotno):
+    shotno = str(shotno)
+    shot_dir = os.path.join('shots', shotno)
     stream = obspy.core.stream.Stream()
     for path in glob.glob(os.path.join(shot_dir, '*.sgy')):
         stream += obspy.read(path, unpack_trace_headers=True)
     return stream
 
-# Current status: The sample data Lindsay sent me doesn't match up
-# with the data I downloaded. Not only are the distances totally
-# different the data itself doesn't seem to match either. I'm going to
-# stop working on this for today because I don't understand what the
-# source and group coordinates are or how the offset distance is
-# calculated and how to correct it.
+def load_station_df():
+    return pd.read_csv('station_lat_lon_elev.csv', index_col=1)
+
+stat_df = load_station_df()
+
+def stat_lat_lon(stat_code):
+    info = stat_df.loc[stat_code]
+    return info.lat, info.lon
+
+def load_shot_df():
+    return pd.read_excel('PASSCAL_SHOT.xlsx', names=['lineno', 'shotno', 'lat', 'lon', 'unknown', 'time', 'linename'], index_col=1, header=None, usecols='A:G')
+
+shot_df = load_shot_df()
+
+def shot_lat_lon(shotno):
+    info = shot_df.loc[shotno]
+    return info.lat, -info.lon  # There seems to be a problem with the shot locations. For now just fix it here.
+
+def source_receiver_offset(tr):
+    # Ignore elevation of receiver.
+    stat_code = tr.stats.segy.trace_header.trace_number_within_the_original_field_record
+    shotno = tr.stats.segy.trace_header.energy_source_point_number
+    dist_m, _, _ = obspy.geodetics.base.gps2dist_azimuth(*stat_lat_lon(stat_code), *shot_lat_lon(shotno))
+    return dist_m
+
+def fix_offsets(old_st):
+    st = obspy.core.Stream()
+    for i in range(len(old_st)):
+        try:
+            tr = old_st[i].copy()
+            tr.stats.segy.trace_header.distance_from_center_of_the_source_point_to_the_center_of_the_receiver_group = source_receiver_offset(tr)
+            st += tr
+        except:
+            pass
+    return st
+
+def plot_all_shots():
+    for shotno in os.listdir('shots'):
+        print(f'loading shot {shotno}')
+        raw_stream = load_shot(shotno)
+        print('...processing')
+        processed_stream = process_stream(fix_offsets(raw_stream))
+        print('...ploting')
+        plt.figure(figsize=(12,9))
+        plot_stream(processed_stream, show=False)
+        fig_path = os.path.join('shot_plots', f'shot_{shotno}.png')
+        plt.savefig(fig_path, dpi=300)
