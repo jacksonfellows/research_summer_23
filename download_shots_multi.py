cb567e4e4e6cbc027a8075ad2cf899b728cdd2c3
Jackson Fellows <jtfellows@icloud.com>
2023-06-06 14:10:37 -0600

    Version of download_shots_multi that retries on error.
    Idk why but when I leave download_shots_multi running I'll sometimes
    get a serialization error from multiprocessing which stops everything.
    Hopefully with this change I'll be able to run downloads overnight.

--- download_shots_multi.py
+++ download_shots_multi.py
@@ -6,3 +6,9 @@ from download_shots import download_shot
 def download_shots_multi(shots):
     with multiprocessing.Pool(8) as p:
         p.map(download_shot, shots, chunksize=1)
+
+def download_shots_multi_retry(shots):
+    try:
+        download_shots_multi(shots)
+    except:
+        download_shots_multi_retry(shots)
