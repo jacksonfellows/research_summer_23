eb0c29a34f0dc21018ffb59091e4f967b3b51a80
Jackson Fellows <jtfellows@icloud.com>
2023-07-07 13:14:12 -0400

    Add utility to project shots to profile back in.
    Now dataclass style.

--- profile_info.py
+++ profile_info.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+import utils
+import numpy as np
+import pandas as pd
+import pygmt
 
 
 @dataclass
@@ -12,6 +16,31 @@ class Profile:
     z1: int
     z2: int
     name: str
+
+    def project_shots(self, shot_nos):
+        """
+        Helper function to project shots to profile.
+        """
+        df = utils._shot_df[utils._shot_df.shotno.isin(shot_nos)]
+        shot_locs = np.column_stack((df.lon, df.lat))
+        projected = pygmt.project(
+            data=shot_locs,
+            center=(self.start_lat_lon[1], self.start_lat_lon[0]),
+            endpoint=(self.end_lat_lon[1], self.end_lat_lon[0]),
+            unit=True,
+        )
+        shot_depth_km = 1e-3 * 12
+        return pd.DataFrame(
+            {
+                # Need the to_numpy() because otherwise pandas tries
+                # to create an index based on the index of the
+                # shot_df.
+                "shotno": df.shotno.to_numpy(),
+                "x": projected[2],
+                "y": projected[3],
+                "z": np.full(len(projected[2]), shot_depth_km),
+            }
+        )
 
 
 profile_1 = Profile(
