e0f6dea3f4525c9b361467689f1a8ef2bb5886cc
Jackson Fellows <jtfellows@icloud.com>
2023-08-03 16:09:23 -0700

    Keep tweaking poster plots.

--- poster_plots.py
+++ poster_plots.py
@@ -13,7 +13,7 @@ def megashot_comparison_plot(shotno, shots_per_side, min_v, max_v):
     stacked_bt = megashot_all_nodes(shotno, shots_per_side, min_v, max_v, "median")
     red_vel = 6.0
     fig, (single_ax, stacked_ax, counts_ax) = plt.subplots(
-        nrows=3, sharex=True, height_ratios=(0.4, 0.4, 0.2), figsize=(8, 12)
+        nrows=3, sharex=True, height_ratios=(0.4, 0.4, 0.2), figsize=(8.5, 12)
     )
     single_bt.plot_mat(
         red_vel=red_vel, vmin=0, show=False, fig=fig, axs=[single_ax, counts_ax]
@@ -29,11 +29,11 @@ def megashot_comparison_plot(shotno, shots_per_side, min_v, max_v):
 
 def megashot_single_plot(shotno, min_v, max_v):
     fig, (shots_ax, stacked_ax) = megashot(shotno, 9001, 1, min_v, max_v, show=False)
-    fig.set_size_inches(8, 6)
     shots_ax.set_xlim(6, 9)
     shots_ax.set_title("Individual")
     stacked_ax.set_title("Stacked")
     shots_ax.set_ylim(-0.1, 1.1)
     stacked_ax.set_ylim(-0.1, 1.1)
     plt.tight_layout()
+    fig.set_size_inches(8, 6)
     plt.savefig("figures/megashot_single.png", dpi=DPI)
