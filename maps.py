f8480445f1dd7ad868391a4b6454a14ce8fbb462
Jackson Fellows <jtfellows@icloud.com>
2023-08-03 16:08:42 -0700

    Update maps for the poster.

--- maps.py
+++ maps.py
@@ -30,71 +30,30 @@ aleut_lines = pd.read_csv("./map_data/aleut_lines.csv")
 edge_lines = geopandas.read_file("./map_data/traced_edge_transect.gpkg")
 avo_stations = pd.read_csv("./map_data/avo_lat_lon_elev.csv")
 
-
-def make_overview_map():
-    fig = pygmt.Figure()
-    region = "US.AK+R10"
-
-    # basemap
-    center_lon = aacse_map_region[0] + (aacse_map_region[1] - aacse_map_region[0]) / 2
-    center_lat = aacse_map_region[2] + (aacse_map_region[3] - aacse_map_region[2]) / 2
-    fig.basemap(
-        projection=f"C{center_lon}/{center_lat}/12c",
-        region=region,
-        frame=["x+10f", "y+5f"],
-    )
-
-    # bathymetry & topography
-    grid = pygmt.datasets.load_earth_relief(resolution="01m", region=region)
-    pygmt.makecpt(cmap="geo", series=(-8000, 8000))
-    fig.grdimage(grid=grid, cmap=True)
-
-    # plate boundaries
-    fig.plot(
-        plate_boundaries,
-        pen="0.5p",
-    )
-
-    # AACSE map location
-    fig.plot(
-        data=[
-            [aacse_map_region[0], aacse_map_region[2]],
-            [aacse_map_region[0], aacse_map_region[3]],
-            [aacse_map_region[1], aacse_map_region[3]],
-            [aacse_map_region[1], aacse_map_region[2]],
-            [aacse_map_region[0], aacse_map_region[2]],
-        ],
-        pen="1p,red",
-        straight_line="mp",
-    )
-
-    fig.savefig(os.path.join(figs_dir, "overview_map.pdf"))
-
-
+model_region = (-155, -150.75, 55.5, 58.5)
 aacse_map_region = (-165, -147, 51, 60)
+
+
+pen_width = "0.015i"
+marker_size = "0.15i"
 
 
 def make_aacse_map():
     fig = pygmt.Figure()
 
     # set area
-    fig.basemap(projection="M12c", region=aacse_map_region, frame="a2f")
+    fig.basemap(projection="M8i", region=aacse_map_region, frame="a2f")
 
     # bathymetry & topography
     grid = pygmt.datasets.load_earth_relief(resolution="15s", region=aacse_map_region)
     pygmt.makecpt(cmap="geo", series=(-8000, 8000))
     fig.grdimage(grid=grid, cmap=True)
 
-    # slab contours
-    fig.plot(
-        "./map_data/alu_slab2_dep_02.23.18_contours.in",
-        pen="0.5p,pink",
-        label="Slab 20 km",
-    )
-
     # faults
     fig.plot(
-        load_usgs_faults_for_region(aacse_map_region), pen="0.02c", label="Mapped Fault"
+        load_usgs_faults_for_region(aacse_map_region),
+        pen=pen_width,
+        label="Mapped Fault",
     )
 
     # rupture zones
@@ -119,20 +78,20 @@ def make_aacse_map():
     for i, row in rupture_zones.iterrows():
         fig.plot(
             rupture_zones.loc[[i]],  # a little silly
-            pen="0.02c,4_4:4p",
+            pen=f"{pen_width},4_4:4p",
             fill=f"{zone_colors[row.rupture_name]}@50",
             label=f"{row.rupture_name} {zone_magnitudes[row.rupture_name]}",
         )
 
     # EDGE lines
-    fig.plot(edge_lines, pen="0.02c,gold", label="EDGE Line")
+    fig.plot(edge_lines, pen=f"{pen_width},gold", label="EDGE Line")
 
     # ALEUT lines
     for i, row in aleut_lines.iterrows():
         fig.plot(
             x=(row.start_longitude, row.stop_longitude),
             y=(row.start_latitude, row.stop_latitude),
-            pen="0.02c,green",
+            pen=f"{pen_width},green",
             label="ALEUT Line" if i == 0 else None,
         )
 
@@ -140,7 +99,7 @@ def make_aacse_map():
     fig.plot(
         x=utils._shot_df.lon,
         y=utils._shot_df.lat,
-        style="c0.02c",
+        style=f"c{pen_width}",
         fill="red",
         label="Airgun Shot+S0.15c",
     )
@@ -149,7 +108,7 @@ def make_aacse_map():
     fig.plot(
         x=utils._volcanoes_df.lon,
         y=utils._volcanoes_df.lat,
-        style="t0.2c",
+        style=f"t{marker_size}",
         fill="black",
         label="Volcano",
     )
@@ -158,7 +117,7 @@ def make_aacse_map():
     fig.plot(
         x=avo_stations.lon,
         y=avo_stations.lat,
-        style="i0.2c",
+        style=f"i{marker_size}",
         pen="black",
         fill="turquoise",
         label="AVO Station",
@@ -168,7 +127,7 @@ def make_aacse_map():
     fig.plot(
         x=utils._node_df.lon,
         y=utils._node_df.lat,
-        style="c0.03c",
+        style=f"c{pen_width}",
         fill="purple",
         label="Nodal Station+S0.15c",
     )
@@ -177,128 +136,87 @@ def make_aacse_map():
     fig.plot(
         x=utils._broadband_df.lon,
         y=utils._broadband_df.lat,
-        style="s0.2c",
+        style=f"s{marker_size}",
         fill="orange",
         pen="black",
         label="Broadband Station",
     )
 
+    # model map
+    fig.plot(
+        data=[
+            [model_region[0], model_region[2]],
+            [model_region[0], model_region[3]],
+            [model_region[1], model_region[3]],
+            [model_region[1], model_region[2]],
+            [model_region[0], model_region[2]],
+        ],
+        pen=f"{pen_width},black",
+        straight_line="mp",
+    )
+
+    # overview inset
+    center_lon = aacse_map_region[0] + (aacse_map_region[1] - aacse_map_region[0]) / 2
+    center_lat = aacse_map_region[2] + (aacse_map_region[3] - aacse_map_region[2]) / 2
+    inset_region = "US.AK+R10"
+    with fig.inset(
+        position="jTL+o0.2c",
+        box="+gwhite+p1p",
+        region=inset_region,
+        projection=f"C{center_lon}/{center_lat}/2i",
+    ):
+        # bathymetry & topography
+        inset_grid = pygmt.datasets.load_earth_relief(
+            resolution="10m", region=inset_region
+        )
+        fig.grdimage(grid=inset_grid, cmap=True)
+
+        # plate boundaries
+        fig.plot(
+            plate_boundaries,
+            pen="0.5p",
+        )
+
+        # AACSE map location
+        fig.plot(
+            data=[
+                [aacse_map_region[0], aacse_map_region[2]],
+                [aacse_map_region[0], aacse_map_region[3]],
+                [aacse_map_region[1], aacse_map_region[3]],
+                [aacse_map_region[1], aacse_map_region[2]],
+                [aacse_map_region[0], aacse_map_region[2]],
+            ],
+            pen=f"{pen_width},black",
+            straight_line="mp",
+        )
+
     # bathymetry/elevation scale
-    with pygmt.config(FONT="8"):
+    with pygmt.config(FONT="12"):
         fig.colorbar(
             cmap="geo",
             truncate=[-8000, 3000],
             frame=["a3000f1000", "x+lBathymetry/Elevation", "y+lm"],
-            position="x0.5c/8c+w3.5c+h",
+            position="x0.25i/5i+w2i+h",
         )
 
     # legend
-    with pygmt.config(FONT="6"):
+    with pygmt.config(FONT="12"):
         fig.legend(position="JBR+jBR+o0.2c", box="+gwhite+p1p")
 
     # scale
-    with pygmt.config(FONT="8"):
+    with pygmt.config(FONT="12"):
         fig.basemap(map_scale="JBL+jBL+o0.5c/0.75+w100")
 
     fig.savefig(os.path.join(figs_dir, "aacse_map.pdf"))
 
 
-def make_aacse_mag_map():
-    fig = pygmt.Figure()
-
-    # set area
-    fig.basemap(projection="M12c", region=aacse_map_region, frame="a2f")
-
-    # magnetic
-    grid = pygmt.datasets.load_earth_magnetic_anomaly(
-        resolution="03m",
-        region=aacse_map_region,
-        data_source="wdmam",
-    )
-    pygmt.makecpt(cmap="mag", series=(-500, 500))
-    fig.grdimage(grid=grid, cmap=True)
-
-    # shorelines
-    fig.coast(region=aacse_map_region, shorelines="1/0.5p")
-
-    # faults
-    fig.plot(
-        load_usgs_faults_for_region(aacse_map_region), pen="0.02c", label="Mapped Fault"
-    )
-
-    # rupture zones
-    fig.plot(
-        rupture_zones,
-        pen="0.02c,4_4:4p",
-        label="Historical Rupture Zone",
-    )
-
-    # magnetic scale
-    with pygmt.config(FONT="8"):
-        fig.colorbar(
-            cmap=True,
-            frame=["a250f125", "x+lMagnetic Anomaly", "y+lnT"],
-            position="x0.5c/8c+w3.5c+h",
-            box="+gwhite+p1p",
-        )
-
-    # scale
-    with pygmt.config(FONT="8"):
-        fig.basemap(map_scale="JBL+jBL+o0.5c/0.75+w100")
-
-    fig.savefig(os.path.join(figs_dir, "aacse_mag_map.pdf"))
-
-
-def make_aacse_grav_map():
-    fig = pygmt.Figure()
-
-    # set area
-    fig.basemap(projection="M12c", region=aacse_map_region, frame="a2f")
-
-    # magnetic
-    grid = pygmt.datasets.load_earth_free_air_anomaly(
-        resolution="01m",
-        region=aacse_map_region,
-    )
-    pygmt.makecpt(cmap="polar", series=(-275, 275))
-    fig.grdimage(grid=grid, cmap=True)
-
-    # shorelines
-    fig.coast(region=aacse_map_region, shorelines="1/0.5p")
-
-    # faults
-    fig.plot(
-        load_usgs_faults_for_region(aacse_map_region), pen="0.02c", label="Mapped Fault"
-    )
-
-    # rupture zones
-    fig.plot(
-        rupture_zones,
-        pen="0.02c,4_4:4p",
-        label="Historical Rupture Zone",
-    )
-
-    # gravity scale
-    with pygmt.config(FONT="8"):
-        fig.colorbar(
-            cmap=True,
-            frame=["a100f50", "x+lFree-Air Anomaly", "y+lmGal"],
-            position="x0.5c/8c+w3.5c+h",
-        )
-
-    # scale
-    with pygmt.config(FONT="8"):
-        fig.basemap(map_scale="JBL+jBL+o0.5c/0.75+w100")
-
-    fig.savefig(os.path.join(figs_dir, "aacse_grav_map.pdf"))
-
-
 def make_model_map():
     fig = pygmt.Figure()
-    region = (-155, -150.75, 55.5, 58.5)
+
+    region = model_region
 
     # set area
-    fig.basemap(projection="M12c", region=region, frame="a1f")
+    fig.basemap(projection="M6i", region=region, frame="a1f")
 
     # bathymetry & topography
     grid = pygmt.datasets.load_earth_relief(resolution="15s", region=region)
@@ -314,7 +232,7 @@ def make_model_map():
     fig.plot(
         x=utils._node_df.lon,
         y=utils._node_df.lat,
-        style="c0.06c",
+        style=f"c{pen_width}",
         fill="purple",
         label="Nodal Station+S0.15c",
     )
@@ -323,7 +241,7 @@ def make_model_map():
     fig.plot(
         x=utils._shot_df.lon,
         y=utils._shot_df.lat,
-        style="c0.06c",
+        style=f"c{pen_width}",
         fill="red",
         label="Airgun Shot+S0.15c",
     )
@@ -332,15 +250,17 @@ def make_model_map():
     fig.plot(
         x=utils._broadband_df.lon,
         y=utils._broadband_df.lat,
-        style="s0.2c",
+        style=f"s{marker_size}",
         fill="orange",
         pen="black",
         label="Broadband Station",
     )
+
     fig.text(
         x=utils._broadband_df.lon,
         y=utils._broadband_df.lat,
         text=utils._broadband_df.code,
+        font="12p",
     )
 
     # profile line
@@ -353,84 +273,16 @@ def make_model_map():
             profile_info.profile_1.start_lat_lon[0],
             profile_info.profile_1.end_lat_lon[0],
         ),
-        pen="0.06c,gold,4_4:4p",
+        pen=f"{pen_width},gold,4_4:4p",
         label="Kodiak Profile",
     )
 
     # legend
-    with pygmt.config(FONT="10"):
+    with pygmt.config(FONT="12"):
         fig.legend(position="JBL+jBL+o0.2c", box="+gwhite+p1p")
 
     # scale
-    with pygmt.config(FONT="8"):
+    with pygmt.config(FONT="12"):
         fig.basemap(map_scale="JBR+jBR+o0.5c/0.75+w50")
 
     fig.savefig(os.path.join(figs_dir, "model_map.pdf"))
-
-
-def make_quake_map():
-    fig = pygmt.Figure()
-    region = (-155, -150, 54.5, 58.5)
-
-    # set area
-    fig.basemap(projection="M12c", region=region, frame="a1f")
-
-    # bathymetry & topography
-    grid = pygmt.datasets.load_earth_relief(resolution="15s", region=region)
-    pygmt.makecpt(cmap="geo", series=(-8000, 8000))
-    fig.grdimage(grid=grid, cmap=True)
-
-    # nodal stations
-    fig.plot(
-        x=utils._node_df.lon,
-        y=utils._node_df.lat,
-        style="c0.06c",
-        fill="purple",
-        label="Nodal Station+S0.15c",
-    )
-
-    # broadband stations
-    fig.plot(
-        x=utils._broadband_df.lon,
-        y=utils._broadband_df.lat,
-        style="s0.2c",
-        fill="orange",
-        pen="black",
-        label="Broadband Station",
-    )
-    fig.text(
-        x=utils._broadband_df.lon,
-        y=utils._broadband_df.lat,
-        text=utils._broadband_df.code,
-    )
-
-    # earthquakes
-    fig.plot(
-        x=utils._earthquake_df.origin_longitude,
-        y=utils._earthquake_df.origin_latitude,
-        style="x0.4c",
-        pen="0.05c,red",
-    )
-    fig.text(
-        x=utils._earthquake_df.origin_longitude,
-        y=utils._earthquake_df.origin_latitude,
-        text=utils._earthquake_df.index,
-    )
-
-    # profile lines
-    for profile, color in (
-        (profile_info.profile_1, "saddlebrown"),
-        (profile_info.profile_2, "limegreen"),
-    ):
-        fig.plot(
-            x=(profile.start_lat_lon[1], profile.end_lat_lon[1]),
-            y=(profile.start_lat_lon[0], profile.end_lat_lon[0]),
-            pen=f"0.06c,{color},4_4:4p",
-            label=profile.name,
-        )
-
-    # legend
-    with pygmt.config(FONT="10"):
-        fig.legend(position="JBL+jBL+o0.2c", box="+gwhite+p1p")
-
-    fig.savefig(os.path.join(figs_dir, "quake_map.pdf"))
